aa043c8ae55827b35499bf9cad908e2344cf3bf8
Carl Kamholtz <carl.kamholtz@gmail.com>
2023-12-09 19:01:12 +1000

    feat: display git status + added some todo notes

--- live_node/lib/live_node_web/live/thermostat_live.ex
+++ live_node/lib/live_node_web/live/thermostat_live.ex
@@ -5,13 +5,46 @@ defmodule LiveNodeWeb.ThermostatLive do
   def render(assigns) do
     ~H"""
     Current temperature: <%= @temperature %>°F
-    <button phx-click="inc_temperature">+</button>
+    <button phx-click="inc_temperature" style="background-color:green">+</button>
+
+    <br>
+
+    <h1>
+      TODO:
+    </h1>
+    
+    - [x] How to send cmd + return output to webpage
+    <div>
+      <h2>Example of using `System.cmd(...)`</h2>
+      <span>Git status: </span><span><%= @git_status %></span>
+    </div>
+    <br>
+
+    - [ ] how to render a collection of `blocks` on the page with particular x,y positions
+    <div>
+      <span>blocks: </span><span><%= @blocks %></span>
+    </div>
+
+
+    - [ ] drag and drop blocks, and assign that new value to the socket
+
+    - [ ] get text from textbox into `handle_event("run_cmd")`
+
     """
   end
 
   def mount(_params, _session, socket) do
     temperature = 70 # Let's assume a fixed temperature for now
-    {:ok, assign(socket, :temperature, temperature)}
+    {:ok, 
+      socket 
+      |> assign(:temperature, temperature)
+      |> assign(:git_status, System.cmd("git", ["status"]) |> elem(0))
+      |> assign(:blocks, [get_block] |> Jason.encode!)
+    }
+  end
+
+  defp get_block() do
+    %{prop: "value"}
   end
 
   def handle_event("inc_temperature", _params, socket) do
