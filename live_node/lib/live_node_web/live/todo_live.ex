45ab97d17d03c2d2a0e514e5b3d6d44988e8f20e
Carl Kamholtz <carl.kamholtz@gmail.com>
2023-12-09 19:05:25 +1000

    refactor: move html into heex file

--- live_node/lib/live_node_web/live/todo_live.ex
+++ live_node/lib/live_node_web/live/todo_live.ex
@@ -2,37 +2,6 @@ defmodule LiveNodeWeb.TodoLive do
   # In Phoenix v1.6+ apps, the line is typically: use LiveNodeWeb, :live_view
   use Phoenix.LiveView
 
-  def render(assigns) do
-    ~H"""
-    Current temperature: <%= @temperature %>°F
-    <button phx-click="inc_temperature" style="background-color:green">+</button>
-
-    <br>
-
-    <h1>
-      TODO:
-    </h1>
-    
-    - [x] How to send cmd + return output to webpage
-    <div>
-      <h2>Example of using `System.cmd(...)`</h2>
-      <span>Git status: </span><span><%= @git_status %></span>
-    </div>
-    <br>
-
-    - [ ] how to render a collection of `blocks` on the page with particular x,y positions
-    <div>
-      <span>blocks: </span><span><%= @blocks %></span>
-    </div>
-
-
-    - [ ] drag and drop blocks, and assign that new value to the socket
-
-    - [ ] get text from textbox into `handle_event("run_cmd")`
-
-    """
-  end
-
   def mount(_params, _session, socket) do
     temperature = 70 # Let's assume a fixed temperature for now
     {:ok, 
