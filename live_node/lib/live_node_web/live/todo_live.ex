3d98b291a09559d8b152b7e7f331f8001fcc6b11
Carl Kamholtz <carl.kamholtz@gmail.com>
2023-12-21 21:56:48 +1000

    feat: delete unused

--- live_node/lib/live_node_web/live/todo_live.ex
+++ live_node/lib/live_node_web/live/todo_live.ex
@@ -14,33 +14,6 @@ defmodule LiveNodeWeb.TodoLive do
     }
   end
 
-  @impl true
-  def update(_, socket) do
-    spec =
-      VegaLite.new(title: "Demo", width: :container, height: :container, padding: 5)
-      # Load values. Values are a map with the attributes to be used by Vegalite
-      |> VegaLite.data_from_values(fake_data())
-      # Defines the type of mark to be used
-      |> VegaLite.mark(:line)
-      # Sets the axis, the key for the data and the type of data
-      |> VegaLite.encode_field(:x, "date", type: :nominal)
-      |> VegaLite.encode_field(:y, "total", type: :quantitative)
-      # Output the specifcation
-      |> VegaLite.to_spec()
-
-    socket = assign(socket, id: socket.id)
-    {:ok, push_event(socket, "vega_lite:#{socket.id}:init", %{"spec" => spec})}
-  end
-
-  defp fake_data do
-    today = Date.utc_today()
-    until = today |> Date.add(10)
-
-    Enum.map(Date.range(today, until), fn date ->
-      %{total: Enum.random(1..100), date: Date.to_iso8601(date), name: "potato"}
-    end)
-  end
-
   defp get_todos() do
     [
       %{title: "first",
