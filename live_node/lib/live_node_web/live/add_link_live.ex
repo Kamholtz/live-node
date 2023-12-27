a3e5e60a2493ed68978aaa053ac98ac581ce5dc4
Carl Kamholtz <carl.kamholtz@gmail.com>
2023-12-27 22:12:04 +1000

    wip:

--- live_node/lib/live_node_web/live/add_link_live.ex
+++ live_node/lib/live_node_web/live/add_link_live.ex
@@ -1,4 +1,12 @@
 defmodule LiveNodeWeb.AddLinkLive do
   use Phoenix.LiveView
   
+
+  def mount(_params, _session, socket) do
+    temperature = 70 # Let's assume a fixed temperature for now
+    {:ok, 
+      socket 
+      |> assign(:abc, "abc")
+    }
+  end
 end
