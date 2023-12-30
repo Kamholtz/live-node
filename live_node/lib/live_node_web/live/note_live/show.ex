df719a5b964fa88ade39292aa281ad673161dd2e
Carl Kamholtz <carl.kamholtz@gmail.com>
2023-12-30 22:59:47 +1000

    feat: getting url param and putting it into assigns in show.ex... might not be necessary

--- live_node/lib/live_node_web/live/note_live/show.ex
+++ live_node/lib/live_node_web/live/note_live/show.ex
@@ -5,15 +5,20 @@ defmodule LiveNodeWeb.NoteLive.Show do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    IO.inspect("HERE1")
+    {:ok, 
+      socket
+      |> assign(:url, "FROM note_live/show.ex")}
   end
 
   @impl true
-  def handle_params(%{"id" => id}, _, socket) do
+  def handle_params(%{"id" => id, "url" => url}, _, socket) do
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:note, Notetaking.get_note!(id))}
+     |> assign(:note, Notetaking.get_note!(id))
+     # |> assign(:url, url)
+    }
   end
 
   defp page_title(:show), do: "Show Note"
