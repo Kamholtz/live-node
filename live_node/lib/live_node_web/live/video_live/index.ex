c254da43640f6d2aa4eae2c8b47b4f19f54ead06
Carl Kamholtz <carl.kamholtz@gmail.com>
2024-01-05 15:36:57 +1000

    fix: compilation

--- live_node/lib/live_node_web/live/video_live/index.ex
+++ live_node/lib/live_node_web/live/video_live/index.ex
@@ -20,17 +20,57 @@ defmodule LiveNodeWeb.VideoLive.Index do
     |> assign(:video, VideoDownload.get_video!(id))
   end
 
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Video")
-    |> assign(:video, %Video{})
-  end
-
   defp apply_action(socket, :index, _params) do
     socket
     |> assign(:page_title, "Listing Videos")
     |> assign(:video, nil)
   end
+
+  defp apply_action(socket, :new, params) do
+    socket
+    |> assign(:page_title, "New Video")
+    |> assign_video_from_url(params)
+    |> apply_url_from_params(params)
+  end
+
+  def assign_url_from_params(socket, %{"url" => url}) do
+    decode_out = Base.url_decode64(url)
+    video =
+      case decode_out do
+        # URL decoded successfully
+        {:ok, u} -> %Video{url: (u)}
+        # FAILURE
+        _ -> %Video{}
+      end
+
+    socket
+    |> assign(:video, video)
+  end
+
+  def assign_video_from_url(socket, _params) do
+    # no url provided case
+    socket
+    |> assign(:video, %Video{})
+  end
+
+
+  defp apply_url_from_params(socket, %{"url" => url}) do
+    IO.inspect(url, label: "url received: ")
+    decoded_url = Base.url_decode64(url)
+
+    case decoded_url do
+      {:ok, u} -> socket
+        |> assign(:url, u)
+      _ -> socket 
+        |> assign(:url, "")
+    end
+  end
+
+  defp apply_url_from_params(socket, _params) do
+    socket
+    |> assign(:url, nil)
+  end
+
 
   @impl true
   def handle_info({LiveNodeWeb.VideoLive.FormComponent, {:saved, video}}, socket) do
